c0aea2cc009b2d464f857a057d2e981f5ddd4a17
matsufriends <pikapikan0526@gmail.com>
2024-10-18 13:07:58 +0900

    ビルド対応

--- String/MornLocalizeString.cs
+++ String/MornLocalizeString.cs
@@ -1,6 +1,8 @@
 ﻿using System;
-using UnityEditor;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace MornLocalize
 {
@@ -21,7 +23,8 @@ namespace MornLocalize
             };
         }
     }
-
+    
+#if UNITY_EDITOR
     [CustomPropertyDrawer(typeof(MornLocalizeString))]
     public class MornLocalizeStringDrawer : PropertyDrawer
     {
@@ -99,4 +102,5 @@ namespace MornLocalize
             }
         }
     }
+#endif
 }
